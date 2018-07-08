87e643ea02679f79c97db91cf19a62d5c5f557ad
DavidAmunga <david.onyimbo@gmail.com>
2018-07-08 16:37:00 +0300

    Added Recipes Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import Recipes from './components/Recipes'
 
 const API_KEY = "e79743444a86e4dab300359a468862af";
 
 class App extends Component {
-  state={
-    recipes:[]
-  }
+  state = {
+    recipes: []
+  };
   getRecipe = async e => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(
-      `http://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`
+      `http://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`
     );
 
     const data = await api_call.json();
 
-    this.setState({recipes:data});
-    
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   };
   render() {
+    const { recipes } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+       
+        <Recipes recipes={recipes}/>
       </div>
     );
   }
